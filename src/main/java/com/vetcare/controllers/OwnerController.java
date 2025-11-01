c6b49add24abd4dbd40733253527b81e9dd11fe0
“Guillermo <g.huaman.o@gmail.com>
2025-11-01 18:24:32 +0100

    Owner Rest Controller Created

--- src/main/java/com/vetcare/controllers/OwnerController.java
+++ src/main/java/com/vetcare/controllers/OwnerController.java
@@ -1,6 +1,14 @@
 package com.vetcare.controllers;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import com.vetcare.models.Owner;
+import com.vetcare.services.OwnerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +17,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @GetMapping
+    public ResponseEntity<List<Owner>> getAllOwners(){
+        return ResponseEntity.ok(ownerService.findAllActive());
+    } 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Owner> getOwnerById(@PathVariable Long id) {
+        return ResponseEntity.ok(ownerService.findById(id));
+    }
+
+    @GetMapping("/{id}/pets")
+    public ResponseEntity<Owner> getOwnerWithPets(@PathVariable Long id){
+        return ResponseEntity.ok(ownerService.findByIdWithPets(id));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Owner>> searchOwners(@RequestParam String name){
+        return ResponseEntity.ok(ownerService.searchByName(name));
+    }
     
+    @PostMapping
+    public ResponseEntity<Owner> createOwner(@Validated @RequestBody Owner owner){
+        Owner createdOwner = ownerService.create(owner);
+
+        return new ResponseEntity<>(createdOwner, HttpStatus.CREATED);
+    }
 }
